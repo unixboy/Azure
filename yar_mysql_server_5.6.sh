7bc05a6b2759b41bf4b09d8097a6a2c62b2dc947
Joel  Shaikin <joel@unixboy.ca>
2017-01-06 15:57:53 -0800

    Update yar_mysql_server_5.6.sh

--- yar_mysql_server_5.6.sh
+++ yar_mysql_server_5.6.sh
@@ -153,11 +153,31 @@ configure_mysql() {
     useradd -r -g mysql mysql
     chmod o+x "${MOUNTPOINT}/mysql"
     chown -R mysql:mysql "${MOUNTPOINT}/mysql"
+ # Download and Install the Latest Updates for the OS
+apt-get update && apt-get upgrade -y
+
+# Set the Server Timezone to CST
+echo "America/Vancouver" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
  
-sudo apt-get update
-echo "mysql-server-5.6 mysql-server/root_password password $mysqlPassword" | sudo debconf-set-selections
-echo "mysql-server-5.6 mysql-server/root_password_again password $mysqlPassword" | sudo debconf-set-selections
-sudo apt-get -y install mysql-server-5.6
+
+# Install essential packages
+apt-get -y install zsh htop
+
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get -y install mysql-server-5.6
+
+
+# Run the MySQL Secure Installation wizard
+mysql_secure_installation
+
+sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
+mysql -uroot -p -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
+
+service mysql restart
 
 
     if [ $iscentos -eq 0 ];
@@ -166,14 +186,7 @@ sudo apt-get -y install mysql-server-5.6
     elif [ $isubuntu -eq 0 ];
     then
 echo installing #sudo apt-get update
-#export DEBIAN_FRONTEND=noninteractive
-#echo "mysql-server-5.6 mysql-server/root_password password $mysqlPassword" | sudo debconf-set-selections
-#echo "mysql-server-5.6 mysql-server/root_password_again password $mysqlPassword" | sudo debconf-set-selections
-#sudo apt-get update
-#sudo mysqladmin -u root password "$mysqlPassword"   #without -p means here the initial password is empty
-
-#sudo service mysql restart
-#echo install_mysql_ubuntu
+ 
    fi
 
 
@@ -185,7 +198,7 @@ echo installing #sudo apt-get update
 #    mysqladmin -u root --password=${mysql_secret} password ${ROOTPWD}
 if [ $iscentos -eq 0 ];
 then
-echo mysql-------mysql
+echo mysql-------mysqlinstalled now
 fi
 }
 
