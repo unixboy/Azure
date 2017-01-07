a9accdf775b0f779a748cdc68ec4c646a3d003d9
Joel  Shaikin <joel@unixboy.ca>
2017-01-06 16:54:47 -0800

    Update yar_mysql_server_5.6.sh

--- yar_mysql_server_5.6.sh
+++ yar_mysql_server_5.6.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-mysqlPassword=$1 
+# This script is only tested on CentOS 6.5 and Ubuntu 12.04 LTS with Percona XtraDB Cluster 5.6.
+# You can customize variables such as MOUNTPOINT, RAIDCHUNKSIZE and so on to your needs.
+# You can also customize it to work with other Linux flavours and versions.
+# If you customize it, copy it to either Azure blob storage or Github so that Azure
+# custom script Linux VM extension can access it, and specify its location in the 
+# parameters of DeployPXC powershell script or runbook or Azure Resource Manager CRP template.   
+
+NODEID=0
+NODEADDRESS=0.0.0.0
+MYCNFTEMPLATE=${3}
+RPLPWD=root
+ROOTPWD=root
+PROBEPWD=root
+MASTERIP=${7}
 
 MOUNTPOINT="/datadrive"
 RAIDCHUNKSIZE=512
 
 RAIDDISK="/dev/md127"
 RAIDPARTITION="/dev/md127p1"
+# An set of disks to ignore from partitioning and formatting
 BLACKLIST="/dev/sda|/dev/sdb"
 
 check_os() {
@@ -35,16 +49,16 @@ scan_for_new_disks() {
 get_disk_count() {
     DISKCOUNT=0
     for DISK in "${DISKS[@]}";
-    do
+    do 
         DISKCOUNT+=1
     done;
     echo "$DISKCOUNT"
 }
 
 create_raid0_ubuntu() {
-    dpkg -s mdadm
+    dpkg -s mdadm 
     if [ ${?} -eq 1 ];
-    then
+    then 
         echo "installing mdadm"
         wget --no-cache http://mirrors.cat.pdx.edu/ubuntu/pool/main/m/mdadm/mdadm_3.2.5-5ubuntu4_amd64.deb
         dpkg -i mdadm_3.2.5-5ubuntu4_amd64.deb
@@ -73,7 +87,7 @@ p
 
 
 w
-" | fdisk "${DISK}"
+" | fdisk "${DISK}" 
 #> /dev/null 2>&1
 
 #
@@ -101,25 +115,25 @@ add_to_fstab() {
 }
 
 configure_disks() {
-        ls "${MOUNTPOINT}"
-        if [ ${?} -eq 0 ]
-        then
-                return
-        fi
+	ls "${MOUNTPOINT}"
+	if [ ${?} -eq 0 ]
+	then 
+		return
+	fi
     DISKS=($(scan_for_new_disks))
     echo "Disks are ${DISKS[@]}"
     declare -i DISKCOUNT
-    DISKCOUNT=$(get_disk_count)
+    DISKCOUNT=$(get_disk_count) 
     echo "Disk count is $DISKCOUNT"
     if [ $DISKCOUNT -gt 1 ];
     then
-        if [ $iscentos -eq 0 ];
-        then
-            create_raid0_centos
-        elif [ $isubuntu -eq 0 ];
-        then
+    	if [ $iscentos -eq 0 ];
+    	then
+       	    create_raid0_centos
+    	elif [ $isubuntu -eq 0 ];
+    	then
             create_raid0_ubuntu
-        fi
+    	fi
         do_partition ${RAIDDISK}
         PARTITION="${RAIDPARTITION}"
     else
@@ -137,7 +151,172 @@ configure_disks() {
     mount "${MOUNTPOINT}"
 }
 
+open_ports() {
+    iptables -A INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+    iptables -A INPUT -p tcp -m tcp --dport 9200 -j ACCEPT
+    iptables-save
+}
 
+disable_apparmor_ubuntu() {
+    /etc/init.d/apparmor stop
+    /etc/init.d/apparmor teardown
+    update-rc.d -f apparmor remove
+    apt-get remove apparmor apparmor-utils -y
+}
+
+disable_selinux_centos() {
+    sed -i 's/^SELINUX=.*/SELINUX=disabled/I' /etc/selinux/config
+    setenforce 0
+}
+
+configure_network() {
+    open_ports
+    if [ $iscentos -eq 0 ];
+    then
+        disable_selinux_centos
+    elif [ $isubuntu -eq 0 ];
+    then
+        disable_apparmor_ubuntu
+    fi
+}
+
+create_mycnf() {
+    wget "${MYCNFTEMPLATE}" -O /etc/1my.cnf
+    sed -i "s/^server_id=.*/server_id=${NODEID}/I" /etc/1my.cnf
+    sed -i "s/^report-host=.*/report-host=${NODEADDRESS}/I" /etc/1my.cnf
+    sed -i "s/^bind-address.*/bind-address=0.0.0.0/I" /etc/mysql/1my.cnf
+}
+
+install_mysql_ubuntu() {
+    dpkg -s mysql-5.6
+    if [ ${?} -eq 0 ];
+    then
+        return
+    fi
+    echo "installing mysql"
+    apt-get update
+    export DEBIAN_FRONTEND=noninteractive
+	apt-get install -y mysql-server-5.6
+	chown -R mysql:mysql "${MOUNTPOINT}/mysql/mysql"
+	apt-get install -y mysql-server-5.6
+	wget http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python_2.1.3-1ubuntu14.04_all.deb
+	dpkg -i mysql-connector-python_2.1.3-1ubuntu14.04_all.deb
+	wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities_1.6.4-1ubuntu14.04_all.deb
+    dpkg -i mysql-utilities_1.6.4-1ubuntu14.04_all.deb
+    apt-get -y install xinetd
+}
+
+install_mysql_centos() {
+    rpm -qa |grep MySQL-server-5.6.26-1.el6.x86_64
+    if [ ${?} -eq 0 ];
+    then
+        return
+    fi
+    echo "installing mysql"
+    wget https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-5.6.26-1.el6.x86_64.rpm-bundle.tar
+    tar -xvf MySQL-5.6.26-1.el6.x86_64.rpm-bundle.tar
+	curlib=$(rpm -qa |grep mysql-libs-)
+    rpm -e --nodeps $curlib
+    rpm -ivh MySQL-server-5.6.26-1.el6.x86_64.rpm
+    rpm -ivh MySQL-client-5.6.26-1.el6.x86_64.rpm
+	wget http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.0.4-1.el6.noarch.rpm
+	rpm -ivh mysql-connector-python-2.0.4-1.el6.noarch.rpm
+	wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-1.5.5-1.el6.noarch.rpm
+	rpm -ivh mysql-utilities-1.5.5-1.el6.noarch.rpm
+    yum -y install xinetd
+}
+
+create_mysql_probe() {
+if [ ${NODEID} -eq 1 ];
+then
+# create a probe user with minimum privilege
+    mysql -u root -p"${ROOTPWD}" <<EOF
+CREATE USER 'probeuser'@'%' IDENTIFIED BY '${PROBEPWD}';
+GRANT SELECT ON *.* TO 'probeuser'@'%';
+FLUSH PRIVILEGES;
+EOF
+fi
+
+# create a probe script
+    cat <<EOF >/usr/bin/mysqlprobe
+#!/bin/bash
+ 
+MYSQL_HOST="${NODEADDRESS}"
+MYSQL_USERNAME="probeuser"
+MYSQL_PASSWORD='${PROBEPWD}'
+
+ERROR_MSG=\`/usr/bin/mysqladmin --host=\${MYSQL_HOST} --port=3306 --user=\${MYSQL_USERNAME} --password=\${MYSQL_PASSWORD} status 2>/dev/null\`
+#ERROR_MSG=\`/usr/bin/mysql --host=\${MYSQL_HOST} --port=3306 --user=\${MYSQL_USERNAME} --password=\${MYSQL_PASSWORD} -e "show databases;" 2>/dev/null\`
+
+if [ "\$ERROR_MSG" != "" ]
+then
+        # mysql is fine, return http 200
+        echo -en "HTTP/1.1 200 OK\r\n"
+        echo -en "Content-Type: Content-Type: text/plain\r\n"
+        echo -en "Connection: close\r\n"
+        echo -en "Content-Length: 19\r\n"
+        echo -en "\r\n"
+        echo -en "MySQL is running.\r\n"
+        sleep 0.1
+        exit 0
+else
+        # mysql is down, return http 503
+        echo -en "HTTP/1.1 503 Service Unavailable\r\n"
+        echo -en "Content-Type: Content-Type: text/plain\r\n"
+        echo -en "Connection: close\r\n"
+        echo -en "Content-Length: 16\r\n"
+        echo -en "\r\n"
+        echo -en "MySQL is down.\r\n"
+        sleep 0.1
+        exit 1
+fi
+EOF
+
+chmod 755 /usr/bin/mysqlprobe
+
+# create a http service that runs the script
+    cat <<EOF >/etc/xinetd.d/mysqlprobe
+# default: on
+# description: mysqlprobe
+service mysqlprobe
+{
+        disable = no
+        flags = REUSE
+        socket_type = stream
+        port = 9200
+        wait = no
+        user = nobody
+        server = /usr/bin/mysqlprobe
+        log_on_failure += USERID
+        only_from = 0.0.0.0/0
+        per_source = UNLIMITED
+}
+EOF
+
+# add the service to xinet
+    grep "mysqlprobe" /etc/services >/dev/null 2>&1
+    if [ ${?} -ne 0 ];
+    then
+        sed -i "\$amysqlprobe  9200\/tcp  #mysqlprobe" /etc/services
+    fi
+    service xinetd restart
+}
+
+configure_mysql_replication() {
+if [ ${NODEID} -eq 1 ];
+then
+    mysql -u root -p"${ROOTPWD}" <<EOF
+CREATE USER 'rpluser'@'%' IDENTIFIED BY '${RPLPWD}';
+GRANT REPLICATION SLAVE ON *.* TO 'rpluser'@'%';
+FLUSH PRIVILEGES;
+EOF
+else
+    mysql -u root -p"${ROOTPWD}" <<EOF
+change master to master_host='${MASTERIP}', master_port=3306, master_user='rpluser', master_password='${RPLPWD}', master_auto_position=1;
+START slave;
+EOF
+fi
+}
 
 configure_mysql() {
     /etc/init.d/mysql status
@@ -153,59 +332,43 @@ configure_mysql() {
     useradd -r -g mysql mysql
     chmod o+x "${MOUNTPOINT}/mysql"
     chown -R mysql:mysql "${MOUNTPOINT}/mysql"
-    chmod -R 775 "${MOUNTPOINT}/mysql"
- # Download and Install the Latest Updates for the OS
-apt-get update 
-
-# Set the Server Timezone to CST
-#echo "America/Vancouver" > /etc/timezone
-#dpkg-reconfigure -f noninteractive tzdata
-
- 
-
-# Install essential packages
-#apt-get -y install htop
-
-# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
-echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
-apt-get -y install mysql-server-5.6
-
- 
 
     if [ $iscentos -eq 0 ];
     then
         install_mysql_centos
     elif [ $isubuntu -eq 0 ];
     then
-echo startingmysql
-service mysql restart
- 
-   fi
+        install_mysql_ubuntu
+    fi
 
-
-
-
-#    create_mycnf
-#    /etc/init.d/mysql restart
-#    mysql_secret=$(awk '/password/{print $NF}' ${HOME}/.mysql_secret)
-#    mysqladmin -u root --password=${mysql_secret} password ${ROOTPWD}
-if [ $iscentos -eq 0 ];
+    create_mycnf
+    /etc/init.d/mysql restart
+    mysql_secret=$(awk '/password/{print $NF}' ${HOME}/.mysql_secret)
+    mysqladmin -u root --password=${mysql_secret} password ${ROOTPWD}
+if [ ${NODEID} -eq 1 ];
 then
-echo mysql-------mysqlinstalled now
+    mysql -u root -p"${ROOTPWD}" <<EOF
+SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('${ROOTPWD}');
+SET PASSWORD FOR 'root'@'::1' = PASSWORD('${ROOTPWD}');
+CREATE USER 'admin'@'%' IDENTIFIED BY '${ROOTPWD}';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' with grant option;
+FLUSH PRIVILEGES;
+EOF
 fi
+    configure_mysql_replication
+    create_mysql_probe
 }
 
 check_os
 if [ $iscentos -ne 0 ] && [ $isubuntu -ne 0 ];
 then
     echo "unsupported operating system"
-    exit 1
+    exit 1 
 else
+    configure_network
     configure_disks
     configure_mysql
-
-
-        #yum -y install microsoft-hyper-v
-#       echo "/sbin/reboot" | /usr/bin/at now + 3 min >/dev/null 2>&1
+	#yum -y erase hypervkvpd.x86_64
+	#yum -y install microsoft-hyper-v
+#	echo "/sbin/reboot" | /usr/bin/at now + 3 min >/dev/null 2>&1
 fi
