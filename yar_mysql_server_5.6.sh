1d117c23e27873dd9e89205db8b6d9379ac78216
Joel  Shaikin <joel@unixboy.ca>
2017-01-28 23:34:48 -0800

    Update yar_mysql_server_5.6.sh

--- yar_mysql_server_5.6.sh
+++ yar_mysql_server_5.6.sh
@@ -183,7 +183,7 @@ configure_network() {
 create_mycnf() {
     echo configtmp 
     #mv /etc/mysql/my.conf /etc/mysql/my.cnf.original
-    wget "${MYCNFTEMPLATE}" -O /etc/mysql/my.cnf
+    wget "${MYCNFTEMPLATE}" -O /etc/mysql/11my.cnf
     #wget "${MYCNFTEMPLATE}" -O /etc/1my.cnf
     #sed -i "s/^server_id=.*/server_id=${NODEID}/I" /etc/1my.cnf
     #sed -i "s/^report-host=.*/report-host=${NODEADDRESS}/I" /etc/my.cnf
